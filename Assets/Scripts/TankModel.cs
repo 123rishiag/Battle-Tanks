6e45a1c47d6c3dca7df3fd2672ba8794c661503b
Rishabh Garg <narishabh.garg@gmail.com>
2024-11-06 22:34:28 +0530

    Implemented movement, rotation to tank and did camera setup

--- Assets/Scripts/TankModel.cs
+++ Assets/Scripts/TankModel.cs
@@ -6,9 +6,13 @@ public class TankModel
 {
     private TankController tankController;
 
-    public TankModel()
-    {
+    public float movementSpeed;
+    public float rotationSpeed;
 
+    public TankModel(float _movementSpeed, float _rotationSpeed)
+    {
+        movementSpeed = _movementSpeed;
+        rotationSpeed = _rotationSpeed;
     }
 
     public void SetTankController(TankController _tankController)
